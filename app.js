8b43ef66245132443741b64d426aee503fa0c3d0
Radoslaw Fabisiak <radoslawfabisiak@gmail.com>
2019-01-08 16:14:35 +0100

    Conect app with DB, CRUD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express      = require("express"),
-    app          = express(),
-    bodyParser   = require("body-parser"),
-    mongoose     = require("mongoose");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/bf_db");
 //***************************************
@@ -23,10 +23,38 @@ app.set("view engine", "ejs");
 
 // SCHEMA SETUP FOR MEASUREMENT
 var measurementSchema = new mongoose.Schema({
-    user: String, date: String, weight: Number, neck: Number, waist: Number, hips: Number,
-    shoulder: Number, chest: Number, arm_lef: Number, arm_right: Number, forearm_left: Number, forearm_right: Number, thigh_left: Number, thigh_right: Number, calve_left: Number, calve_right: Number, wrist_left: Number, wrist_right: Number,
-    pectoral: Number, abdominal: Number, thigh: Number, triceps: Number, suprailiac: Number, chest_s: Number, midaxilary: Number, subscapula: Number, biceps: Number, calf: Number, lowerback: Number,
-    annotations: String, img_front: String, img_side: String
+    user: String,
+    date: String,
+    weight: Number,
+    neck: Number,
+    waist: Number,
+    hips: Number,
+    shoulder: Number,
+    chest: Number,
+    arm_lef: Number,
+    arm_right: Number,
+    forearm_left: Number,
+    forearm_right: Number,
+    thigh_left: Number,
+    thigh_right: Number,
+    calve_left: Number,
+    calve_right: Number,
+    wrist_left: Number,
+    wrist_right: Number,
+    pectoral: Number,
+    abdominal: Number,
+    thigh: Number,
+    triceps: Number,
+    suprailiac: Number,
+    chest_s: Number,
+    midaxilary: Number,
+    subscapula: Number,
+    biceps: Number,
+    calf: Number,
+    lowerback: Number,
+    annotations: String,
+    img_front: String,
+    img_side: String
 });
 
 var Measurement = mongoose.model("Measurement", measurementSchema);
@@ -55,48 +83,55 @@ var Measurement = mongoose.model("Measurement", measurementSchema);
 //    }
 // });
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("landing")
 });
 
-app.get("/measurements", function (req, res){
-    res.render("measurements", {measurements:measurements})
+app.get("/measurements", function (req, res) {
+    // Get all measurements form DB
+    Measurement.find({}, function (err, allMeasurements) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("measurements", {measurements: allMeasurements})
+        }
+    })
 });
 
-app.post("/measurements", function (req, res){
+app.post("/measurements", function (req, res) {
     // Getting data from form
-    var user            = req.body.user          ;
-    var date            = req.body.date          ;
-    var weight          = req.body.weight        ;
-    var neck            = req.body.neck          ;
-    var waist           = req.body.waist         ;
-    var hips            = req.body.hips          ;
-    var shoulder        = req.body.shoulder      ;
-    var chest           = req.body.chest         ;
-    var arm_lef         = req.body.arm_lef       ;
-    var arm_right       = req.body.arm_right     ;
-    var forearm_left    = req.body.forearm_left  ;
-    var forearm_right   = req.body.forearm_right ;
-    var thigh_left      = req.body.thigh_left    ;
-    var thigh_right     = req.body.thigh_right   ;
-    var calve_left      = req.body.calve_left    ;
-    var calve_right     = req.body.calve_right   ;
-    var wrist_left      = req.body.wrist_left    ;
-    var wrist_right     = req.body.wrist_right   ;
-    var pectoral        = req.body.pectoral      ;
-    var abdominal       = req.body.abdominal     ;
-    var thigh           = req.body.thigh         ;
-    var triceps         = req.body.triceps       ;
-    var suprailiac      = req.body.suprailiac    ;
-    var chest_s         = req.body.chest_s       ;
-    var midaxilary      = req.body.midaxilary    ;
-    var subscapula      = req.body.subscapula    ;
-    var bicep           = req.body.bicep         ;
-    var calf            = req.body.calf          ;
-    var lowerback       = req.body.lowerback     ;
-    var annotations     = req.body.annotations   ;
-    var img_front       = req.body.img_front     ;
-    var img_side        = req.body.img_side      ;
+    var user = req.body.user;
+    var date = req.body.date;
+    var weight = req.body.weight;
+    var neck = req.body.neck;
+    var waist = req.body.waist;
+    var hips = req.body.hips;
+    var shoulder = req.body.shoulder;
+    var chest = req.body.chest;
+    var arm_lef = req.body.arm_lef;
+    var arm_right = req.body.arm_right;
+    var forearm_left = req.body.forearm_left;
+    var forearm_right = req.body.forearm_right;
+    var thigh_left = req.body.thigh_left;
+    var thigh_right = req.body.thigh_right;
+    var calve_left = req.body.calve_left;
+    var calve_right = req.body.calve_right;
+    var wrist_left = req.body.wrist_left;
+    var wrist_right = req.body.wrist_right;
+    var pectoral = req.body.pectoral;
+    var abdominal = req.body.abdominal;
+    var thigh = req.body.thigh;
+    var triceps = req.body.triceps;
+    var suprailiac = req.body.suprailiac;
+    var chest_s = req.body.chest_s;
+    var midaxilary = req.body.midaxilary;
+    var subscapula = req.body.subscapula;
+    var bicep = req.body.bicep;
+    var calf = req.body.calf;
+    var lowerback = req.body.lowerback;
+    var annotations = req.body.annotations;
+    var img_front = req.body.img_front;
+    var img_side = req.body.img_side;
 
     var newMeasurement = {
         user: user,
@@ -132,17 +167,22 @@ app.post("/measurements", function (req, res){
         img_front: img_front,
         img_side: img_side,
     };
+    // Create new measurement and save to DB
+    Measurement.create(newMeasurement, function (err, newlyCreated) {
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("measurements")
+        }
 
-    measurements.push(newMeasurement);
+    });
 
-    // Redirect to measurements page
-    res.redirect("measurements")
 });
 
-app.get("/measurements/new", function (req, res){
+app.get("/measurements/new", function (req, res) {
     res.render("new_measurement");
 });
 
-app.listen(3000, function (){
+app.listen(3000, function () {
     console.log("The bodyfat server has started on port 3000. Press ctrl + C to disconnect");
 });
